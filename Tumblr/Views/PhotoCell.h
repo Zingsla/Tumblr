bc26e2f25d625b5a06485ef0db47c6bbc31712bd
Jacob Franz <franzj@fb.com>
2020-06-25 11:59:51 -0500

    Fetches photos from Tumblr API and displays them in a TableView

---
+++ Tumblr/Views/PhotoCell.h
@@ -0,0 +1,20 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Jacob Franz on 6/25/20.
+//  Copyright © 2020 Jacob Franz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
